b8c36149b5b3df5551781f3815c7a9d057695855
Rady DevOps <40141281+DevOps1-A@users.noreply.github.com>
2025-07-19 19:08:47 +0200

    Update ciscotool.py

--- ciscotool.py
+++ ciscotool.py
@@ -1,2 +1,5 @@
-def show_version():
-    return "Cisco IOS XE Software, Version 17.3.3"
+from netmiko import ConnectHandler
+
+def get_show_version(device_params):
+    with ConnectHandler(**device_params) as net_connect:
+        return net_connect.send_command("show version")
